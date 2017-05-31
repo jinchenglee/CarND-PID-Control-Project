755e1faba2fac9e7eaae0ab43756d1fac563030b
jinchenglee <jincheng.li@gmail.com>
2017-05-31 00:05:20 -0700

    Initial tuning on PID.

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,9 +11,19 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+    PID::Kp = Kp;
+    PID::Ki = Ki;
+    PID::Kd = Kd;
+    p_error = 0.0;
+    d_error = 0.0;
+    i_error = 0.0;
 }
 
 void PID::UpdateError(double cte) {
+    // Delta from last cte which is held by p_error
+    d_error = cte - p_error;
+    p_error = cte;
+    i_error += cte;
 }
 
 double PID::TotalError() {
--- src/main.cpp
+++ src/main.cpp
@@ -34,6 +34,7 @@ int main()
 
   PID pid;
   // TODO: Initialize the pid variable.
+  pid.Init(0.08, 0.00002, 0.05);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -57,13 +58,25 @@ int main()
           * NOTE: Feel free to play around with the throttle and speed. Maybe use
           * another PID controller to control the speed!
           */
+          pid.UpdateError(cte);
+
+          steer_value = -pid.Kp*pid.p_error -pid.Kd*pid.d_error - pid.Ki*pid.i_error;
           
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
 
+          // development phase, capped at low speed
+          double throttle = 0.3;
+          if(speed > 30.0)
+              throttle = 0.0;
+
+          // Slow down in sharp turns
+          if((fabs(steer_value)>0.1) && (speed > 15.0))
+              throttle = -0.3;
+
           json msgJson;
           msgJson["steering_angle"] = steer_value;
-          msgJson["throttle"] = 0.3;
+          msgJson["throttle"] = throttle;
           auto msg = "42[\"steer\"," + msgJson.dump() + "]";
           std::cout << msg << std::endl;
           ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
